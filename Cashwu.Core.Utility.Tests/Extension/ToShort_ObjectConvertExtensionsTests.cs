aaf60faedc43599f3f06c476f3dfb38466f14cd3
Cash Wu <cash@cashwu.com>
2019-08-12 10:22:40 +0800

    add ToShort ObjectConvert test

--- Cashwu.Core.Utility.Tests/Extension/ToShort_ObjectConvertExtensionsTests.cs
+++ Cashwu.Core.Utility.Tests/Extension/ToShort_ObjectConvertExtensionsTests.cs
@@ -5,102 +5,103 @@ using Xunit;
 
 namespace Cashwu.Core.Utility.Tests.Extension
 {
-    public class ToByteObjectConvertExtensionsTests
+    public class ToShortObjectConvertExtensionsTests
     {
         [Fact]
         public void Null()
         {
-            ((object)null).ToByte().Should().Be(0);
+            ((object)null).ToShort().Should().Be(0);
         }
 
         [Fact]
         public void Null_default_value()
         {
-            ((object)null).ToByte(1).Should().Be(1);
+            ((object)null).ToShort(1).Should().Be(1);
         }
 
         [Fact]
         public void Empty_string()
         {
-            "".ToByte().Should().Be(0);
+            "".ToShort().Should().Be(0);
         }
 
         [Fact]
         public void Not_empty_string_not_numeric()
         {
-            "a".ToByte().Should().Be(0);
+            "a".ToShort().Should().Be(0);
         }
 
         [Fact]
         public void Not_empty_string_is_numeric()
         {
-            "2".ToByte().Should().Be(2);
+            "2".ToShort().Should().Be(2);
         }
 
         [Fact]
         public void Char_not_numeric()
         {
-            'a'.ToByte().Should().Be(0x61);
+            'a'.ToShort().Should().Be(0x61);
         }
 
         [Fact]
         public void Char_is_numeric()
         {
-            '1'.ToByte().Should().Be(0x31);
+            '1'.ToShort().Should().Be(0x31);
+        }
+
+        [Fact]
+        public void Byte()
+        {
+            byte b = byte.MaxValue;
+            b.ToShort().Should().Be(255);
         }
 
         [Fact]
         public void Int()
         {
             int i = 8;
-            i.ToByte().Should().Be(8);
+            i.ToShort().Should().Be(8);
         }
 
         [Fact]
         public void Int_more_than_byte_size()
         {
             int i = int.MaxValue;
-            Func<byte> action = () => i.ToByte();
+            Func<short> action = () => i.ToShort();
 
             action.Should()
                   .Throw<OverflowException>()
                   .And
                   .Message.Should()
-                  .Be("Value was either too large or too small for an unsigned byte.");
+                  .Be("Value was either too large or too small for an Int16.");
         }
 
         [Fact]
         public void Int_negative()
         {
             int i = -10;
-            Func<byte> action = () => i.ToByte();
-            
-            action.Should()
-                  .Throw<OverflowException>()
-                  .And
-                  .Message.Should()
-                  .Be("Value was either too large or too small for an unsigned byte.");
+            i.ToShort().Should().Be(-10);
         }
 
         [Fact]
         public void Long()
         {
             long l = 9;
-            l.ToByte().Should().Be(9);
+            l.ToShort().Should().Be(9);
         }
 
         [Fact]
         public void Double()
         {
             double d = 8.1;
-            d.ToByte().Should().Be(8);
+            d.ToShort().Should().Be(8);
         }
 
         [Fact]
         public void Float()
         {
             float f = 7.1f;
-            f.ToByte().Should().Be(7);
+            f.ToShort().Should().Be(7);
         }
     }
 }
